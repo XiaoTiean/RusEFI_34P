227c6d41e5d09649e7c12ed6703921efaa559585
XiaoTiean <124254796+XiaoTiean@users.noreply.github.com>
2025-12-17 15:53:22 +0700

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -18,7 +18,7 @@ static void customBoardDefaultConfiguration() {
     // engineConfiguration->injectionPins[0] = Gpio::F13;
     // engineConfiguration->ignitionPins[0] = Gpio::E15;
 
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
+//  engineConfiguration->triggerInputPins[0] = Gpio::B1;
 //	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
 
 //	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
@@ -28,33 +28,17 @@ static void customBoardDefaultConfiguration() {
 //	engineConfiguration->iat.adcChannel = EFI_ADC_2;
 
 
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+    // 470 high side/910 low side = 1.56 ratio divider
+ 	engineConfiguration->analogInputDividerCoefficient = 1.936f;
 
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
-
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
-
-
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
-}
-
-static void setupVbatt() {
-	// 5.6k high side/10k low side = 1.56 ratio divider
-	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+    // 10k high side/ 1k low side
+  	engineConfiguration->vbattDividerCoeff = (10 + 1) / 1;
 	
-	// 6.34k high side/ 1k low side
-	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1; 
-
-	// Battery sense on PA7
-	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
-
+	// ADC voltage referent
 	engineConfiguration->adcVcc = 3.3f;
+
+	engineConfiguration->clt.config.bias_resistor = 2400;
+	engineConfiguration->iat.config.bias_resistor = 2400;
 }
 
 void setup_custom_board_overrides() {
