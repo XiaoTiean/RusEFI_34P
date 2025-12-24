06650be7ad71261e45819c6c930fb2e053decd5a
GitHub git update Action <action@github.com>
2025-12-24 02:04:05 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_RusEFI_34P.h
+++ generated/controllers/generated/rusefi_generated_RusEFI_34P.h
@@ -2137,7 +2137,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.12.23.RusEFI_34P.806443901"
+#define TS_SIGNATURE "rusEFI main.2025.12.24.RusEFI_34P.806443901"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
