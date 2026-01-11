48649ce89ee8f84e98e2776eeecc71c8b0a8b398
XiaoTiean <124254796+XiaoTiean@users.noreply.github.com>
2026-01-11 21:40:26 +0700

    Update 34P_meta.h

--- 34P_meta.h
+++ 34P_meta.h
@@ -1,22 +1,22 @@
 #pragma once
 //LSHC = Low side high current (1A continue on Rusefi_34P1)
-#define 34P_LSHC_1 E10
-#define 34P_LSHC_2 E9
-#define 34P_LSHC_3 E8
-#define 34P_LSHC_4 E7
+#define P34_LSHC_1 E10
+#define P34_LSHC_2 E9
+#define P34_LSHC_3 E8
+#define P34_LSHC_4 E7
 //LSHV = Low side high voltage (up to 350v) can drive ignigtion coil
-#define 34P_LSHV_1 E12
-#define 34P_LSHV_2 E11
-#define 34P_LSHV_3 E14
-#define 34P_LSHV_4 E13
+#define P34_LSHV_1 E12
+#define P34_LSHV_2 E11
+#define P34_LSHV_3 E14
+#define P34_LSHV_4 E13
 // VR (MAX9926)
-#define 34P_VR_1 B1
-#define 34P_VR_2 B0
+#define P34_VR_1 B1
+#define P34_VR_2 B0
 // 5V GPIO 
-#define 34P_GPIO_1 D13
-#define 34P_GPIO_2 D12
-#define 34P_GPIO_3 B15
-#define 34P_GPIO_4 B14
-#define 34P_GPIO_5 B10
-#define 34P_GPIO_6 B11
+#define P34_GPIO_1 D13
+#define P34_GPIO_2 D12
+#define P34_GPIO_3 B15
+#define P34_GPIO_4 B14
+#define P34_GPIO_5 B10
+#define P34_GPIO_6 B11
 
